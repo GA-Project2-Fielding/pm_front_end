67119a648e620aad66ba167699914d4a8db0764e
abroccoli <acbrocco@syr.edu>
2015-03-15 20:20:36 -0400

    commented out some of the main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,28 +1,28 @@
 'use strict';
 console.log('main loaded');
 
-var PM = (function (module) {
-  var host = 'http://localhost:3000/';
+// var PM = (function (module) {
+//   var host = 'http://localhost:3000/';
 
-  module.apiRoutes = {
-    users: host + 'users/',
-    projects: host + 'projects/',
-    tasks: host + 'tasks/',
-    comments: host + 'comments/',
-    fileLocations: host + 'file_locations/'
-  };
+//   module.apiRoutes = {
+//     users: host + 'users/',
+//     projects: host + 'projects/',
+//     tasks: host + 'tasks/',
+//     comments: host + 'comments/',
+//     fileLocations: host + 'file_locations/'
+//   };
 
-  return module;
-})(PM || {});
-
-
+//   return module;
+// })(PM || {});
 
 
 
-// $(document).ready(function() {
-//   $.getScript('scripts/user.js');
-//   $.getScript('scripts/project.js');
-//   $.getScript('scripts/task.js');
-//   $.getScript('scripts/comment.js');
-//   $.getScript('scripts/fileLocation.js');
-// });
+
+
+$(document).ready(function() {
+  $.getScript('scripts/user.js');
+  // $.getScript('scripts/project.js');
+  // $.getScript('scripts/task.js');
+  // $.getScript('scripts/comment.js');
+  // $.getScript('scripts/fileLocation.js');
+});
