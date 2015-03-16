b51372397c820a441800da06f96827a068d107dd
Caitlyn Landry <caitlyn.landry@gmail.com>
2015-03-16 15:18:59 -0400

    Add preliminary AJAX calls for tasks

--- app/scripts/task.js
+++ app/scripts/task.js
@@ -12,17 +12,52 @@ var PM = (function (module) {
     fileLocations: host + 'file_locations/'
   };
 
-//   module.getTasks = function() {
-//   $.ajax({
-//     url: module.apiRoutes.tasks,
-//     type: 'GET'
-//   }).done(function(data) {
-//     var template = Handlebars.compile($('#movies-template').html());
-//     $('#movies').html(template(data));
-//   }).fail(function(jqXHR, textStatus, errorThrown) {
-//     console.log(jqXHR, textStatus, errorThrown);
-//   });
-// };
+  module.getTasks = function() {
+    $.ajax({
+      url: module.apiRoutes.projects,
+      type: 'GET',
+      headers: { 'AUTHORIZATION': 'Token token=' + module.authToken }
+    }).done(function(data) {
+      console.log(data);
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+      console.log(jqXHR, textStatus, errorThrown);
+    });
+  };
+
+  module.getOneTask = function(id) {
+    $.ajax({
+      url: module.apiRoutes.tasks + id,
+      type: 'GET',
+      headers: { 'AUTHORIZATION': 'Token token=' + module.authToken }
+    }).done(function(data) {
+       console.log(data);
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+      console.log(jqXHR, textStatus, errorThrown);
+    });
+  };
+
+  module.createTask = function(event) {
+    event.preventDefault();
+    $.ajax({
+      url: module.apiRoutes.projects + id + '/tasks',
+      type: 'POST',
+      headers: { 'AUTHORIZATION': 'Token token=' + module.authToken },
+      data: { task: {
+        due_date: $('input#').val(),
+        completed: $('input#').val(),
+        priority: $('input#').val(),
+        title: $('input#').val(),
+        description: $('input#').val()
+        }
+      }
+    }).done(function(data) {
+      console.log(data);
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+      console.log(jqXHR, textStatus, errorThrown);
+    });
+  };
 
   return module;
 })(PM || {});
+
+PM.getTasks();
