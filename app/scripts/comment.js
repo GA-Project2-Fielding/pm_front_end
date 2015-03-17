61c2f39be85a9d3d2db7a0e0b5fbd48e9b35a1ad
abroccoli <acbrocco@syr.edu>
2015-03-17 19:12:54 -0400

    added comment create field to task show page

--- app/scripts/comment.js
+++ app/scripts/comment.js
@@ -42,5 +42,20 @@ var PM = (function (module) {
     });
   };
 
+  module.createComment = function(){
+    $.ajax({
+      url: '/path/to/file',
+      type: 'default GET (Other values: POST)',
+      dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
+      data: {param1: 'value1'},
+    })
+    .done(function() {
+      console.log("success");
+    })
+    .fail(function() {
+      console.log("error");
+    });
+  };
+
   return module;
 })(PM || {});
