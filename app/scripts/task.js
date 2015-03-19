8d86660413f7e188d34867ad5555f03d5637f0a0
abroccoli <acbrocco@syr.edu>
2015-03-19 11:13:15 -0400

    debugged creation of tasks and subtasks

--- app/scripts/task.js
+++ app/scripts/task.js
@@ -62,15 +62,15 @@ var PM = (function (module) {
       type: 'POST',
       headers: { 'AUTHORIZATION': 'Token token=' + authToken },
       data: { task: {
-        due_date: $('input#taskDueDate').val(),
-        completed: $('input#taskCompleted').is(':checked'),
-        priority: $('select#taskPriority').val(),
-        title: $('input#taskTitle').val(),
-        description: $('textarea#taskDescription').val()
+        due_date: $('.new-task-form .taskDueDate').val(),
+        completed: $('.new-task-form .taskCompleted').is(':checked'),
+        priority: $('.new-task-form .taskPriority').val(),
+        title: $('.new-task-form .taskTitle').val(),
+        description: $('.new-task-form .taskDescription').val()
         }
       }
-    }).done(function(data) {
-      window.location.href = '/#/tasks/'+ data.id;
+    }).done(function() {
+      window.location.href = '/#/projects/'+ projectId;
     }).fail(function(jqXHR, textStatus, errorThrown) {
       console.log(jqXHR, textStatus, errorThrown);
     });
@@ -123,20 +123,21 @@ var PM = (function (module) {
 
   module.createSubtask = function(event) {
     event.preventDefault();
+    taskId = localStorage.getItem('taskId');
     $.ajax({
       url: module.apiRoutes.tasks + taskId + '/subtasks',
       type: 'POST',
       headers: { 'AUTHORIZATION': 'Token token=' + authToken },
       data: { task: {
-        due_date: $('input#taskDueDate').val(),
-        completed: $('input#taskCompleted').is(':checked'),
-        priority: $('select#taskPriority').val(),
-        title: $('input#taskTitle').val(),
-        description: $('textarea#taskDescription').val()
+        due_date: $('.new-subtask-form .taskDueDate').val(),
+        completed: $('.new-subtask-form .taskCompleted').is(':checked'),
+        priority: $('.new-subtask-form .taskPriority').val(),
+        title: $('.new-subtask-form .taskTitle').val(),
+        description: $('.new-subtask-form .taskDescription').val()
         }
       }
-    }).done(function(data) {
-      window.location.href = '/#/tasks/'+ data.id;
+    }).done(function() {
+      window.location.href = '/#/tasks/'+ taskId;
     }).fail(function(data) {
       console.log(data);
     });
